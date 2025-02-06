7f76c4e4bf7d9b35738db65a17695b218c10e56f
Eduard <me@edwardcode.net>
2025-02-06 17:25:12 +1000

    Fix checking format

--- src/main/java/net/edwardcode/fishbot/Utils.java
+++ src/main/java/net/edwardcode/fishbot/Utils.java
@@ -3,7 +3,7 @@ package net.edwardcode.fishbot;
 public class Utils {
     public static int validateMessage(String message) {
         // 1.1. Check if message ends with fish
-        if (!message.endsWith("\uD83D\uDC1F")) {
+        if (!message.endsWith(" \uD83D\uDC1F")) {
             return -1;
         }
         // 1.2. Check if message starts with numbers
